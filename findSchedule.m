3ef0a7596a68bc9783a594552b918c9dc09edfbe
Dmitry Sinyukov <Dmitry Sinyukov>
2016-01-31 20:59:09 +0200

    initial commit

---
+++ findSchedule.m
@@ -0,0 +1,10 @@
+function [ schedule ] = findSchedule( tasksInfo, processorsInfo )
+%findSchedule tries find the optimal schedule in polynomial time
+%   tasksInfo - information describing tasks
+%   processorsInfo - information describing processors
+%   schedule - resulting schedule
+
+schedule = 0;
+
+end
+
