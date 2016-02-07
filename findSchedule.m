f878943b6a0426faf6405569a3e4869fa8b5908d
Dmitry Sinyukov <Dmitry Sinyukov>
2016-02-08 00:22:07 +0200

    Forgotten file.

--- findSchedule.m
+++ findSchedule.m
@@ -1,10 +1,41 @@
 function [ schedule ] = findSchedule( tasksInfo, processorsInfo )
-%findSchedule tries find the optimal schedule in polynomial time
+%findSchedule tries to find the optimal schedule in polynomial time
 %   tasksInfo - information describing tasks
 %   processorsInfo - information describing processors
 %   schedule - resulting schedule
 
-schedule = 0;
-
+% Check arguments
+if ~exist('tasksInfo','var') || ~exist('processorsInfo','var') || ...
+        ~isnumeric(tasksInfo) || ~isnumeric(processorsInfo) || ...
+        isempty(tasksInfo) || isempty(processorsInfo) || ...
+        size(tasksInfo, 2) < 2 || (size(processorsInfo, 2) ~= 1 && ...
+        size(tasksInfo, 1) ~= size(processorsInfo, 1))
+    disp('Arguments error.');
+    return;
 end
 
+schedule = {};
+
+if any(tasksInfo(:,2) < tasksInfo(:,1))
+    return;
+end
+
+% sorting by d - l
+preparedTasks = [(1:size(tasksInfo, 1))' tasksInfo (tasksInfo(:, 2) - tasksInfo(:, 1))];    
+preparedTasks = sortrows(preparedTasks, 4);
+
+if size(processorsInfo, 2) == 1
+    % Same performances processors
+    initSchedule = initialSchedule(preparedTasks, processorsInfo(1));
+    if ~isempty(initSchedule)
+        % a_1_1, a_1_2, ...
+        
+    end
+    % Approximation
+    return;
+end
+
+% Different performances processors
+disp('Different performances processors.');
+
+end
